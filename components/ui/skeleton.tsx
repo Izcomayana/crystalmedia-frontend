5fda72cc9e55356304381836a918e71a3080cfbe
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2024-07-26 12:00:28 +0100

    fix skeleton

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -6,7 +6,7 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
+      className={cn("animate-pulse rounded-md bg-[#d4d4d4]", className)}
       {...props}
     />
   )
